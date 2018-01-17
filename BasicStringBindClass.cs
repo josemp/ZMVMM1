11eb13eff284033ebe14ed98354f62bb5f451fa6
Jose Miguel Parra <josem.parra@gmail.com>
2018-01-17 18:34:11 +0100

    String conversiones con convert y format

--- BasicStringBindClass.cs
+++ BasicStringBindClass.cs
@@ -10,25 +10,25 @@ namespace zmvvm1
     /// <summary>
     /// 
     /// </summary>
-    public delegate bool validaDel<T>(T value, ref string sError);
+    //public delegate bool validaStringDel(String value, ref string sError);
 
-    public class BasicBindClass<T> : ViewModelBindBaseClass
+    public class BasicStringBindClass<T> : ViewModelBindBaseClass
     {
         //public NotifyDataErrorInfoClass notifyDataErrorInfo;
-        public BasicBindClass()
+        public BasicStringBindClass()
         {
-
+           
         }
 
-        public BasicBindClass(T valor)
+        public BasicStringBindClass(T valor)
         {
             _data = valor;
         }
-        public BasicBindClass(NotifyDataErrorInfoDictionaryClass notifyDataErrorInfo, validaDel<T> valida, CommandClass comando)
+        public BasicStringBindClass(NotifyDataErrorInfoDictionaryClass notifyDataErrorInfo, validaDel<T> valida,CommandClass comando)
         {
             // this.basicStringBind();
             this.notifyDataErrorInfo = notifyDataErrorInfo;
-
+           
             this.valida = valida;
             this.comando = comando;
         }
@@ -42,16 +42,16 @@ namespace zmvvm1
             _data = valor;
         }
         */
-        public BasicBindClass(NotifyDataErrorInfoDictionaryClass notifyDataErrorInfo, validaDel<T> valida, bool esObligatorio, CommandClass comando)
+        public BasicStringBindClass(NotifyDataErrorInfoDictionaryClass notifyDataErrorInfo, validaDel<T> valida, bool esObligatorio,CommandClass comando)
         {
             // this.basicStringBind();
             this.notifyDataErrorInfo = notifyDataErrorInfo;
-
+           
             this.valida = valida;
             this.esObligatorio = esObligatorio;
             this.comando = comando;
         }
-        public BasicBindClass(NotifyDataErrorInfoDictionaryClass notifyDataErrorInfo, validaDel<T> valida, bool esObligatorio, T valor, CommandClass comando)
+        public BasicStringBindClass(NotifyDataErrorInfoDictionaryClass notifyDataErrorInfo, validaDel<T> valida, bool esObligatorio, T valor, CommandClass comando)
         {
             // this.basicStringBind();
             this.notifyDataErrorInfo = notifyDataErrorInfo;
@@ -67,44 +67,82 @@ namespace zmvvm1
         }
 
         string nameProperty = "b_data";
+        string namePropertyFormat = "f_data";
         String sError;
         bool esObligatorio = false;
-        CommandClass comando = null;
+        CommandClass comando=null;
         private validaDel<T> valida = null;
         private T _data { get; set; }
-        private T _b_data { get; set; }
+        private String _b_data { get; set; }
+        public IzConvert<T> convert;
+
+        public String f_data
+        {
+            get
+            {
+                if (notifyDataErrorInfo != null && notifyDataErrorInfo.HasErrorProperty(nameProperty) == true) return (_b_data);
+                else
+                {
+                    return convert.formatear(data, b_data);
+                    return (b_data);
+                }
+            }
+
+        }
 
         // binded propertye
-        public T b_data
+
+        public String b_data
         {
-            get { if (notifyDataErrorInfo != null && notifyDataErrorInfo.HasErrorProperty(nameProperty) == true) return (_b_data); else  return (_data); }
+            get
+            {
+                if (notifyDataErrorInfo != null && notifyDataErrorInfo.HasErrorProperty(nameProperty) == true) return (_b_data);
+                else
+                {
+                    string x=convert.convertToString(_data);
+                    return x;
+                }
+            }
             set
             {
+                String sError="";
 
 
-                // if (valida != null)
+               // if (valida != null)
                 {
+                    String valor = value;
+                    if (convert.valida(valor,ref sError)==false)
+                    {
+                        _b_data = valor;
+                        RaiseErrorsChanged(nameProperty, sError);
+                        if (comando != null)
+                            comando.notifyChange();
+                        NotifyOfPropertyChanged(namePropertyFormat);
+                        return;
 
-                    T valor = value;
+                    }
+
+                    
                     if (esObligatorio == true)
                     {
-                        if (valor == null
-                            || (typeof(T) == typeof(String) && valor.ToString() == "")
-                            || (typeof(T) == typeof(string) && valor.ToString() == "")
-                            )
+                        if (valor == null 
+                            || valor == "")
+                            
+                            
                         {
                             _b_data = valor;
                             RaiseErrorsChanged(nameProperty, "Dato obligatorio");
-                            if (comando != null)
+                            if (comando != null) 
                                 comando.notifyChange();
+                            NotifyOfPropertyChanged(namePropertyFormat);
                             return;
                         }
                     }
-
-                    if (valida == null || valida(valor, ref sError) == true)
+                    T tValor = convert.convert(valor);
+                    if (valida==null || valida(tValor, ref sError) == true)
                     {
 
-                        _data = valor;
+                        _data = tValor;
                         ClearErrors(nameProperty);
                         if (comando != null)
                             comando.notifyChange();
@@ -118,6 +156,7 @@ namespace zmvvm1
                         if (comando != null) comando.notifyChange();
                         // Cuidado con valores recursivos
                     }
+                    NotifyOfPropertyChanged(namePropertyFormat);
 
                 }
             }
@@ -128,8 +167,10 @@ namespace zmvvm1
             get { return (_data); }
             set
             {
-                b_data = value;
+                b_data = convert.convertToString(value);
                 NotifyOfPropertyChanged(nameProperty);
+                NotifyOfPropertyChanged(namePropertyFormat);
+
             }
         }
     }
---
+++ ConvertClass.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace zmvvm1
+{
+    public interface IzConvert<T>
+    {
+       // Regex regex;
+       // String format;
+        bool valida(String value);
+        bool valida(String value, ref String sError);
+        T convert(String value);
+        
+        String convertToString(T value);
+        String formatear(T value,String value1);
+    }
+    
+    public static class zConvert
+    {
+        public static IzConvert<Double?> doubleBasic = new DoubleBasic();
+        public static IzConvert<DateTime?> dateTimeBasic = new DateTimeBasic();
+    }
+
+    public class DoubleBasic : IzConvert<Double?>
+    {
+         
+        Regex regex = new Regex("^(([0-9]*)|(([0-9]*)\\.([0-9]*)))$");
+        string format = "{0:#0.00}";
+        public DoubleBasic(){}
+
+        public DoubleBasic(Regex regex,string format)
+        {
+            this.regex=regex;
+            this.format=format;
+        }
+        //True if correct
+        public bool match(String value)
+        {
+            if (regex == null)
+                return (true);
+            try
+            {
+                return (regex.IsMatch(value));
+            }
+            catch(Exception e)
+            {
+                return (false);
+            }
+
+        }
+        public bool valida(String value, ref String sError)
+        {
+
+            Double x;
+
+            if (match(value) == false) { sError = "Caracteres no permitidos"; return (false); }
+            bool b = Double.TryParse(value, System.Globalization.NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, System.Globalization.CultureInfo.InvariantCulture, out x);
+            if (b == false)
+                sError = "No se pudo convertir a número";
+            return (b);
+        }
+        public bool valida(String value)
+        {
+            string sError = "";
+            return valida(value, ref sError);
+        }
+        public Double? convert(String value)
+        {
+            Double x;
+            if (value == null) return (null);
+            bool b = Double.TryParse(value, System.Globalization.NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, System.Globalization.CultureInfo.InvariantCulture, out x);
+            Double xx;
+           // xx=(Double)Convert.ChangeType((x.ToString(), typeof(Double));
+            return x;
+        }
+        public String convertToString(Double? data)
+        {
+            if (data == null)
+                return (null);
+           
+            String x = ((Double)data).ToString(CultureInfo.InvariantCulture);
+            return x;
+        }
+       public String formatear(Double? data,String data1)
+        {
+            if (data==null) return(null);
+           if (format==null) return(data1);
+           return String.Format(format, data);
+        }
+        /*
+        public static T GetValue<T>(String value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+         * */
+
+
+
+    }
+    public class DateTimeBasic : IzConvert<DateTime?>
+    {
+
+        Regex regex = null;
+        public string format = "{0:dd/MM/yyyy}";
+        public string editMask = "ddMMyy";
+        public string formatEdit()
+        {
+            return "{0:" + editMask +"}";
+        }
+        public DateTimeBasic() { }
+
+        public DateTimeBasic(Regex regex, string format)
+        {
+            this.regex = regex;
+            this.format = format;
+        }
+        //True if correct
+        public bool match(String value)
+        {
+            if (regex == null)
+                return (true);
+            try
+            {
+                return (regex.IsMatch(value));
+            }
+            catch (Exception e)
+            {
+                return (false);
+            }
+
+        }
+        public bool valida(String value, ref String sError)
+        {
+
+            DateTime x;
+
+            if (match(value) == false) { sError = "Caracteres no permitidos"; return (false); }
+            bool b = DateTime.TryParseExact(
+                   value,
+                   editMask,
+                   CultureInfo.InvariantCulture,
+                   DateTimeStyles.None,
+                   out x);
+            if (b == false)
+                sError = "No se pudo convertir a número";
+            return (b);
+        }
+        public bool valida(String value)
+        {
+            string sError = "";
+            return valida(value, ref sError);
+        }
+        public DateTime? convert(String value)
+        {
+            DateTime x;
+            if (value == null) return (null);
+            //bool b = DateTime.TryParse(value, "ddMMyy", out x);
+            bool b =DateTime.TryParseExact(
+                   value,
+                   editMask,
+                   CultureInfo.InvariantCulture,
+                   DateTimeStyles.None,
+                   out x);
+           return x;
+        }
+        public String convertToString(DateTime? data)
+        {
+            if (data == null)
+                return (null);
+            string f=formatEdit();
+            String x = String.Format(formatEdit(), data);
+            return x;
+            //String x = ((Double)data).ToString(CultureInfo.InvariantCulture);
+            //return x;
+        }
+        public String formatear(DateTime? data, String data1)
+        {
+            if (data == null) return (null);
+            if (format == null) return (data1);
+            return String.Format(format, data);
+        }
+        /*
+        public static T GetValue<T>(String value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+         * */
+
+
+
+    }
+    public class StringBasic : IzConvert<String>
+    {
+
+        Regex regex = null;
+        String format = null;
+        //True if correct
+        public bool match(String value)
+        {
+            if (regex == null)
+                return (true);
+            try
+            {
+                return (regex.IsMatch(value));
+            }
+            catch (Exception e)
+            {
+                return (false);
+            }
+
+        }
+        public bool valida(String value, ref String sError)
+        {
+
+            
+            if (match(value) == false) { sError = "Caracteres no permitidos"; return (false); }
+            return (true);
+        }
+        public bool valida(String value)
+        {
+            string sError = "";
+            return valida(value, ref sError);
+        }
+        public String convert(String value)
+        {
+            return (value);
+        }
+        public String convertToString(String data)
+        {
+            return data;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data">Data</param>
+        /// <param name="data1">b_data, binding data</param>
+        /// <returns></returns>
+        public String formatear(String data,String data1)
+        {
+            return (data1);
+        }
+
+    }
+}
+
